d628cbb650e7212466b0c232f141b5e3b0bd6cd5
A.Klein <allenkleined@gmail.com>
2022-12-01 16:00:40 -0800

    added logic for indexedDB to add to the db and get all

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => 
+{
+  const jatetextDB = await openDB('jate', 1);
+  const tx = jatetextDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ id: 1, value: content });
+  const result = await request;
+
+  console.log('Text successfully saved to database',result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+  const jatetextDB = await openDB('jate', 1);
+  const tx = jatetextDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
